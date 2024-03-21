18b30357a38bbfc6055e76b56e5868cb39900399
cardoso012 <lucas.cardoso1228@gmail.com>
2024-03-21 08:28:17 -0300

    primeiro commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const path = require('path');
+const cors = require('cors');
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+app.set('views', path.join(__dirname, 'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.get('/', (req, res) => {
+    res.render('index.html');
+});
+
+const messages = [];
+io.on('connection', socket => {
+    console.log(`Socket conectado: ${socket.id}`);
+
+    socket.on('sendMessage', data => {
+        messages.push(data);
+        socket.broadcast.emit('receivedMessage', data);
+    });
+
+    socket.emit('previousMessages', messages);
+});
+
+httpServer.listen(5000);
